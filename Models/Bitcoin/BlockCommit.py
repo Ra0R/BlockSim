47cd36935db0ff029ed81845c607de1ae4a89e45
Renato <renato.rao@bluewin.ch>
2023-04-18 09:02:12 +0200

    + Forking logic for bitcoin model

--- Models/Bitcoin/BlockCommit.py
+++ Models/Bitcoin/BlockCommit.py
@@ -9,9 +9,12 @@ from Scheduler import Scheduler
 from Statistics import Statistics
 
 
+def profile(f): return f
+
 class BlockCommit(BaseBlockCommit):
 
     # Handling and running Events
+    @profile
     def handle_event(event):
         if event.type == "create_block":
             BlockCommit.generate_block(event)
@@ -19,68 +22,86 @@ class BlockCommit(BaseBlockCommit):
             BlockCommit.receive_block(event)
 
     # Block Creation Event
-    def generate_block (event):
+    @profile
+    def generate_block(event):
         miner = InputsConfig.NODES[event.block.miner]
         minerId = miner.id
         eventTime = event.time
         blockPrev = event.block.previous
 
         if blockPrev == miner.last_block().id:
-            Statistics.totalBlocks += 1 # count # of total blocks created!
+            Statistics.totalBlocks += 1  # count # of total blocks created!
             if InputsConfig.hasTrans:
-                if InputsConfig.Ttechnique == "Light": blockTrans,blockSize = LT.execute_transactions()
-                elif InputsConfig.Ttechnique == "Full": blockTrans,blockSize = FT.execute_transactions(miner,eventTime)
+                if InputsConfig.Ttechnique == "Light":
+                    blockTrans, blockSize = LT.execute_transactions()
+                elif InputsConfig.Ttechnique == "Full":
+                    blockTrans, blockSize = FT.execute_transactions(miner, eventTime)
 
                 event.block.transactions = blockTrans
-                event.block.usedgas= blockSize
+                event.block.usedgas = blockSize
 
             miner.blockchain.append(event.block)
 
-            if InputsConfig.hasTrans and InputsConfig.Ttechnique == "Light":LT.create_transactions() # generate transactions
+
+            if InputsConfig.hasTrans and InputsConfig.Ttechnique == "Light":
+                LT.create_transactions()  # generate transactions
 
             BlockCommit.propagate_block(event.block)
-            BlockCommit.generate_next_block(miner,eventTime)# Start mining or working on the next block
+            # Start mining or working on the next block
+            BlockCommit.generate_next_block(miner, eventTime)
+            
 
     # Block Receiving Event
-    def receive_block (event):
+    @profile
+    def receive_block(event):
 
         miner = InputsConfig.NODES[event.block.miner]
         minerId = miner.id
         currentTime = event.time
-        blockPrev = event.block.previous # previous block id
+        blockPrev = event.block.previous  # previous block id
 
-
-        node = InputsConfig.NODES[event.node] # recipint
-        lastBlockId= node.last_block().id # the id of last block
+        node : Node = InputsConfig.NODES[event.node]  # recipint
+        lastBlockId = node.last_block().id  # the id of last block
 
         #### case 1: the received block is built on top of the last block according to the recipient's blockchain ####
         if blockPrev == lastBlockId:
-            node.blockchain.append(event.block) # append the block to local blockchain
-            if InputsConfig.hasTrans and InputsConfig.Ttechnique == "Full": BlockCommit.update_transactionsPool(node, event.block)
-            BlockCommit.generate_next_block(node,currentTime)# Start mining or working on the next block
+            # append the block to local blockchain
+            node.blockchain.append(event.block)
+            if InputsConfig.hasTrans and InputsConfig.Ttechnique == "Full":
+                BlockCommit.update_transactionsPool(node, event.block)
+            # Start mining or working on the next block
+            BlockCommit.generate_next_block(node, currentTime)
 
          #### case 2: the received block is  not built on top of the last block ####
         else:
             depth = event.block.depth + 1
             if (depth > len(node.blockchain)):
-                BlockCommit.update_local_blockchain(node,miner,depth)
-                BlockCommit.generate_next_block(node,currentTime)# Start mining or working on the next block
+                BlockCommit.update_local_blockchain(node, miner, depth)
+                # Start mining or working on the next block
+                BlockCommit.generate_next_block(node, currentTime)
+            else:
+                # if the block is not built on top of the last block, it is a forked block
+                node.forkedBlocks.append(event.block)
 
-            if InputsConfig.hasTrans and InputsConfig.Ttechnique == "Full": BlockCommit.update_transactionsPool(node,event.block) # not sure yet.
+            if InputsConfig.hasTrans and InputsConfig.Ttechnique == "Full":
+                # not sure yet.
+                BlockCommit.update_transactionsPool(node, event.block)
 
     # Upon generating or receiving a block, the miner start working on the next block as in POW
-    def generate_next_block(node,currentTime):
-	    if node.hashPower > 0:
-                 blockTime = currentTime + c.Protocol(node) # time when miner x generate the next block
-                 Scheduler.create_block_event(node,blockTime)
+    def generate_next_block(node, currentTime):
+        if node.hashPower > 0:
+            # time when miner x generate the next block
+            blockTime = currentTime + c.Protocol(node)
+            Scheduler.create_block_event(node, blockTime)
 
     def generate_initial_events():
-            currentTime=0
-            for node in InputsConfig.NODES:
-            	BlockCommit.generate_next_block(node,currentTime)
+        currentTime = 0
+        for node in InputsConfig.NODES:
+            BlockCommit.generate_next_block(node, currentTime)
 
-    def propagate_block (block):
+    def propagate_block(block):
         for recipient in InputsConfig.NODES:
             if recipient.id != block.miner:
-                blockDelay= Network.block_prop_delay() # draw block propagation delay from a distribution !! or you can assign 0 to ignore block propagation delay
-                Scheduler.receive_block_event(recipient,block,blockDelay)
+                # draw block propagation delay from a distribution !! or you can assign 0 to ignore block propagation delay
+                blockDelay = Network.block_prop_delay()
+                Scheduler.receive_block_event(recipient, block, blockDelay)
