cbb4f12ac1dcbf3ddbc7704a1042bcfcb6d91449
Renato <renato.rao@bluewin.ch>
2023-08-15 13:27:02 +0200

    r Removed block_delay from combinations

--- src/ParameterSpace.py
+++ src/ParameterSpace.py
@@ -2,11 +2,11 @@ import itertools
 
 
 class ParameterSpace:
-    block_creation_per_second = [0.5, 1, 2, 50, 600]
+    block_creation_interval = [0.5, 1, 2, 50, 600]
     tx_delay = [0.150, 2, 5, 10, 15]
     block_delay = [tx_delay[0]*2, tx_delay[1]*2, tx_delay[2]*2, tx_delay[3]*1.5, tx_delay[4]]  # In relation to tx_delay
 
-    combinations = list(itertools.product(block_creation_per_second, tx_delay, block_delay))
+    combinations = list(itertools.product(block_creation_interval, tx_delay))
     
     def __init__(self):
         pass
@@ -15,4 +15,5 @@ class ParameterSpace:
         return self.combinations.pop()
 
     def get_combination(self, index) :
-        return self.combinations[index]
+        # return combination and append block delay
+        return self.combinations[index] + (self.block_delay[index],)
