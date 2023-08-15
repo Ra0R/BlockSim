fa72109e3fc1a4cd1db3cc6b780bf92292769503
Renato <renato.rao@bluewin.ch>
2023-08-15 14:11:23 +0200

    r Updated param space

--- src/ParameterSpace.py
+++ src/ParameterSpace.py
@@ -5,7 +5,7 @@ class ParameterSpace:
     block_creation_interval = [0.5, 1, 2, 50, 600]
     tx_delay = [0.150, 2, 5, 10, 15]
     # Defines the block delay multiplier in regards to transaction delay
-    block_delay_multiplier = [1, 2, 3]
+    block_delay_multiplier = [1, 2, 3, 4, 5]
     combinations = list(itertools.product(block_creation_interval, tx_delay, block_delay_multiplier))
 
     def __init__(self):
