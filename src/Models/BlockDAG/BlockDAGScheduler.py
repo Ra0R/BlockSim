b854b5921d958cc047d7837fb640f918212137d9
Renato <renato.rao@bluewin.ch>
2023-08-15 13:28:32 +0200

    ! Now ensures that referenced blocks contain at least one valid transaction

--- src/Models/BlockDAG/BlockDAGScheduler.py
+++ src/Models/BlockDAG/BlockDAGScheduler.py
@@ -33,17 +33,24 @@ class BlockDAGScheduler:
                     if miner.blockDAG.is_referenced(forkedBlock.id):
                        continue
 
+                    # Check if forkedBlock contains any transaction that is not already included in the chain
+                    includeable_tx = False
+                    for tx in forkedBlock.transactions:
+                        if not miner.blockDAG.contains_tx(tx):
+                            includeable_tx = True
+                            break
+                        
+                    if not includeable_tx:
+                        continue
+
                     if not miner.blockDAG.is_in_chain_of_block(block.previous, forkedBlock.id):
                         print("Block " + str(forkedBlock.id) +  " is not in the chain of block " + str(block.previous))
                         references.append(forkedBlock.id)
                         included_blocks.append(forkedBlock)
 
+
                 ref_copy = references.copy()
 
-                print("---- CHECKING REFERENCES " + str(block.id) +" WITH LENGTH " + str(len(references)) + "----")
-                if (block.id == 60204964071):
-                        miner.blockDAG.plot()
-                        input("Press Enter to continue...")
                 # Check if after adding a reference, the block is in the chain of another reference
                 for ref1 in references:
                     graph_sim = miner.blockDAG.simluate_add_block(block.id, block.previous, [ref1])
