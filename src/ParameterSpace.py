94a3cf05eca632161500cc2ac55e82be7381934c
Renato <renato.rao@bluewin.ch>
2023-10-18 09:16:38 +0200

    Updated parameter space

--- src/ParameterSpace.py
+++ src/ParameterSpace.py
@@ -1,13 +1,13 @@
 import itertools
 
 class ParameterSpace:
-    #block_creation_interval = [0.5, 1, 2, 50, 600]
-    block_creation_interval = [0.5, .75, 1, 2, 3, 5, 10]
+    block_creation_interval = [0.5, 1, 2, 50, 600]
+    #block_creation_interval = [0.5, .75, 1, 2, 3, 5, 10]
     #tx_delay = [0.150, 2, 5, 10, 15, 30]
     tx_delay = [0.5, 5, 10 , 15, 30, 60]
     # Defines the block delay -> this may produce some non-sensical combinations
     # where block_delay < tx_delay, but this is not a problem
-    block_delay = [5]
+    block_delay = [0.3, 4, 10, 20, 30]
     
     combinations = list(itertools.product(block_creation_interval, tx_delay, block_delay))
 
